679a0f83c8c6cb442d712170939643ed5cef7cde
ovi <oaparfene@gmail.com>
2022-12-22 16:40:18 +0200

    mui restart + react router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Table from "./components/Table";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div>
-        <Table />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
